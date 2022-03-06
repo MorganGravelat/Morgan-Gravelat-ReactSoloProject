b27659d24177ceff5d3414d11f3d2d59d5432176
MorganGravelat <mgravelat@gmail.com>
2022-03-05 23:08:06 -0500

    Redux signup feature implemented basic functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,24 @@
 // frontend/src/App.js
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { Route, Switch } from 'react-router-dom';
+import { useDispatch } from "react-redux";
 import LoginFormPage from './components/LoginFormPage';
+import * as sessionActions from "./store/session";
 
 function App() {
-  return (
-    <Switch>
-      <Route path="/login">
-        <LoginFormPage />
-      </Route>
-    </Switch>
-  );
-}
+    const dispatch = useDispatch();
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+      dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+    }, [dispatch]);
 
-export default App;
+    return isLoaded && (
+      <Switch>
+        <Route path="/login">
+          <LoginFormPage />
+        </Route>
+      </Switch>
+    );
+  }
+
+  export default App;
